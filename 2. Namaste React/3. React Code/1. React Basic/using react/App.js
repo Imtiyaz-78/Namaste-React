4031f8cc6c10473443d2336cba97d79b1c6fddda
Imtiyazdeveloper <imtiyazmd803@gmail.com>
2023-01-04 01:12:55 +0530

    updated

--- 2. Namaste React/3. React Code/1. React Basic/using react/App.js
+++ 2. Namaste React/3. React Code/1. React Basic/using react/App.js
@@ -1,7 +1,35 @@
-const heading = React.creatElement(
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement(
     "h1",
     {
-        id : "title",
+        id: "title",
     },
-    "Heading"
+    "Heading 1 From parcel"
 );
+
+const heading2 = React.createElement(
+    "h1",
+    {
+        id: "title",
+    },
+    "Heading 2"
+);
+
+const container = React.createElement(
+    "div",
+    {
+        id: "container",
+        hello: "world",
+    },
+    // [heading1, heading2]
+);
+
+console.log(heading1);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container)
---
+++ 2. Namaste React/3. React Code/1. React Basic/using react/dist/index.6bd02f5a.js
@@ -0,0 +1,6 @@
+const heading = React.creatElement("h1", {
+    id: "title"
+}, "Heading");
+const heading2 = Rea;
+
+//# sourceMappingURL=index.6bd02f5a.js.map
